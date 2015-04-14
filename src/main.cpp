9c8f67b2e01f9a371d4364ffe3448b9f8eae9895
Timothy Truong <ttruo018@ucr.edu>
2015-04-14 14:00:42 -0700

    main.cpp can find every connector

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <unistd.h>
 #include <string>
 #include <iostream>
+#include <boost/tokenizer.hpp>
 
 using namespace std;
+using namespace boost;
 
 //will parse the commands passed in 
 //and will output a vector of char**
@@ -13,18 +15,19 @@ using namespace std;
 void runexec(char** argv);	//will use system calls to run the 
 							//commands provided
 
-char findconnect(int cnt, char** str);	//will determine the next connector
+//string findconnect(string str);	//outputs a string of every connector
 
-int findchar(int cnt, char** line, char c);
-//finds a char index from a char**
+//string parseonce(string str);		//will create the first token from the original string
 
 int main(int argc, char** argv) {
 	string input;
-	while(input!="exit") {
+	bool end = false;
+	while(!end) {
 		cout << "$ ";
 		getline(cin, input);
-		//cout << "output: " << findconnect(argc, argv) << endl;
-
+		string connect = findconnect(input);
+		cout << "connect: " << connect << endl;
+		
 	}
 	//first we need to parse once by 
 	//using find() to determine the closest
@@ -44,37 +47,93 @@ void runexec(char** argv) {
 
 }
 
-char findconnect(int cnt, char** str) {
-	int orcon = findchar(cnt, str, '|'); 
-	cout << "line" << endl;
-	int andcon = findchar(cnt, str, '&'); 
-	int semicon = findchar(cnt, str, ';');
+string findconnect(string str) {
+	string out;
+	int orcon = str.find("||"); 
+	int andcon = str.find("&&"); 
+	int semicon = str.find(";");
 	
-	if(orcon==-1 && andcon==-1 && semicon==-1) {
-		return '1';		//if all equal to -1, it means that 
-	}					//there are no more connectors
+	if(orcon==-1 && andcon==-1 && semicon==-1)	//if all equal to -1, it means that 
+		return "";								//there are no more connectors
 
-	if(orcon<andcon) { //checks which connector is the closest
-		if(orcon<semicon) 
-			return '|';
-		else
-			return ';';
-	}
-	else if(andcon<semicon) 
-			return '&';
-		else
-			return ';';
-}
-
-int findchar (int cnt, char** line, char c) {
-	for(int i=0; i<cnt; i++) {
-		char* str = line[i];
-		for(int j=0; str[j]!=0; j++) {
-			cout << str[j] ;
-			if(str[j]==c)
-				return i;
+	if(andcon==-1) {	//if there is no "&&"
+		if(semicon==-1) {
+			out.push_back('|');
+			str.erase(orcon,2);
 		}
-		cout << endl;
+		else if(orcon==-1) {
+			out.push_back(';');
+			str.erase(semicon,1);
+		}
+		else if(orcon<semicon) {
+			out.push_back('|');
+			str.erase(orcon,2);
+		}
+		else {
+			out.push_back(';');
+			str.erase(semicon,1);
+		}
 	}
-	return -1;
+	else if(semicon==-1) {	//if there is no ";"
+		if(andcon==-1) {
+			out.push_back('|');
+			str.erase(orcon,2);
+		}
+		else if(orcon==-1) {
+			out.push_back('&');
+			str.erase(andcon,2);
+		}
+		else if(orcon<andcon) {
+			out.push_back('|');
+			str.erase(orcon,2);
+		}
+		else {
+			out.push_back('&');
+			str.erase(andcon,2);
+		}
+	}
+	else if(orcon==-1) {	//if there is no "||"
+		if(andcon==-1) {
+			out.push_back(';');
+			str.erase(semicon,1);
+		}
+		else if(semicon==-1) {
+			out.push_back('&');
+			str.erase(andcon,2);
+		}
+		else if(andcon<semicon) {
+			out.push_back('&');
+			str.erase(andcon,2);
+		}
+		else {
+			out.push_back(';');
+			str.erase(semicon,1);
+		}
+	}
+	else if(orcon<andcon) { //checks which connector is the closest if all are inputted
+		if(orcon<andcon) {
+			if(orcon<semicon) {
+				out.push_back('|');
+				str.erase(orcon,2);
+			}
+			else {
+				out.push_back(';');
+				str.erase(semicon);
+			}
+		}
+	}
+	else if(andcon<semicon) {
+		out.push_back('&');
+		str.erase(andcon,2);
+	}
+	else {
+		out.push_back(';');
+		str.erase(semicon,1);
+	}
+	out = out + findconnect(str);
+	return out;
 }
+
+//string parseonce(string str) {
+//
+//}
