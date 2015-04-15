3eb5f59131a2e0bedbc4ef73247d5d6b6d353e7e
Timothy Truong <ttruo018@ucr.edu>
2015-04-15 14:37:40 -0700

    can separate commands by connectors

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <iostream>
 #include <boost/tokenizer.hpp>
+#include <queue>
 
 using namespace std;
 using namespace boost;
@@ -15,9 +16,11 @@ using namespace boost;
 void runexec(char** argv);	//will use system calls to run the 
 							//commands provided
 
-//string findconnect(string str);	//outputs a string of every connector
+string findconnect(string str);		//outputs a string of every connector
 
-//string parseonce(string str);		//will create the first token from the original string
+queue<string> parseline(string str);		//separate commands by connectors from the original string
+
+//char* parsecommand(string str);	//parse the spaces from each command and output as char*
 
 int main(int argc, char** argv) {
 	string input;
@@ -25,9 +28,13 @@ int main(int argc, char** argv) {
 	while(!end) {
 		cout << "$ ";
 		getline(cin, input);
-		string connect = findconnect(input);
-		cout << "connect: " << connect << endl;
-		
+		string connect = findconnect(input);	//connect stores every connector
+		queue<string> cmds = parseline(input);
+		while(!cmds.empty()) {
+			cout << "command: " << cmds.front() << endl;
+			cmds.pop();
+		}
+			
 	}
 	//first we need to parse once by 
 	//using find() to determine the closest
@@ -123,8 +130,7 @@ string findconnect(string str) {
 		}
 	}
 	else if(andcon<semicon) {
-		out.push_back('&');
-		str.erase(andcon,2);
+		out.push_back('&'); str.erase(andcon,2);
 	}
 	else {
 		out.push_back(';');
@@ -134,6 +140,21 @@ string findconnect(string str) {
 	return out;
 }
 
-//string parseonce(string str) {
-//
+queue<string> parseline(string str) {
+	queue<string> out;
+	char_separator<char> delim(";&|");
+	tokenizer< char_separator<char> > token(str, delim);
+	
+	for(tokenizer< char_separator<char> >::iterator i=token.begin(); i!=token.end(); ++i) {
+		out.push(*i);
+	}
+	return out;
+}
+
+//char** parsecommand(string str) {
+//	char** out;	
+//	tokenizer<> token(str);
+//	for(tokenizer<>::iterator i=token.begin(); i!=token.end(); ++i) {
+//		
+//	}
 //}
